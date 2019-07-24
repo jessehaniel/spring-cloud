d9a903848408b11c3550aa41fe86f2cf83a5544d
06071721423 <jesse.haniel@tjpb.jus.br>
2019-07-24 11:13:04 -0300

    adding boot-admin application

---
+++ boot-admin/src/main/java/dev/jessehaniel/springcloud/bootadmin/BootAdminApplication.java
@@ -0,0 +1,15 @@
+package dev.jessehaniel.springcloud.bootadmin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAdminServer
+public class BootAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootAdminApplication.class, args);
+    }
+
+}
